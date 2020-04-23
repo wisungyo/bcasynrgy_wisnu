916175bec99d06978fa37bfa501f64864173b0ea
wisungyo <wisungyo@gmail.com>
2020-04-23 23:10:15 +0700

    minor updates

--- 01_basic_class/session_09/js/script.js
+++ 01_basic_class/session_09/js/script.js
@@ -6,6 +6,13 @@ function addList() {
     para.classList.add("alert-secondary");
     para.appendChild(t);
 
+    // var buttons = '<div class="btn-group" role="group" aria-label="Basic example">' + 
+    //               '<button type="button" class="btn btn-info">v</button>' +
+    //               '<button type="button" class="btn btn-secondary">x</button>' +
+    //               'button type="button" class="btn btn-dark">x</button>' +
+    //               '</div>';
+    // addElement('idButton',);
+
     if (inputValue === '') {
         alert("You must write something!");
     } else {
